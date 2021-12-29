adeb476dc2462e7fe38e05f0e443aa28821833f9
ricardo-emanuel24 <ricardo.emanuel@ufpe.br>
2021-12-29 02:18:40 -0300

    root

--- rootsearch.py
+++ rootsearch.py
@@ -24,7 +24,7 @@ def f(x): return x**3 - 10.0*x**2 + 5.0
 x1 = 0.0; x2 = 1.0
 for i in range(4):
     dx = (x2 - x1)/10.0
-    x1,x2 = searchingroot2(f,x1,x2,dx)
+    x1,x2 = searchingroot(f,x1,x2,dx)
 
 x = (x1 + x2)/2.0
 print('x =', '{:6.4f}'.format(x))
