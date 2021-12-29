04c7d61967292d148241dc1f70dccdd30fccdeaa
ricardo-emanuel24 <ricardo.emanuel@ufpe.br>
2021-12-29 11:03:43 -0300

    r

--- rootsearch.py
+++ rootsearch.py
@@ -1,23 +1,16 @@
 from numpy import sign
 
 def searchingroot(f,x1,x2,dx):
-    while x1<x2:
+    f1=f(x1)
+    f2=f(x1+dx)
+
+    while sign(f1) == sign(f2):
+        if x1 >= x2: return None,None
+        x1 = x1 + dx
         f1=f(x1)
         f2=f(x1+dx)
-        if sign(f1) != sign(f2):
-            return x1, (x1+dx)
-        x1 = x1 + dx
 
-
-def searchingroot2(f,a,b,dx):
-    x1 = a; f1 = f(a)
-    x2 = a + dx; f2 = f(x2)
-    while sign(f1) == sign(f2):
-        if x1 >= b: return None,None
-        x1 = x2; f1 = f2
-        x2 = x1 + dx; f2 = f(x2)
-
-    return x1,x2
+    return x1, (x1+dx)
 
 def f(x): return x**3 - 10.0*x**2 + 5.0
 
@@ -25,6 +18,10 @@ x1 = 0.0; x2 = 1.0
 for i in range(4):
     dx = (x2 - x1)/10.0
     x1,x2 = searchingroot(f,x1,x2,dx)
+    if x1 == None and x2 == None:
+        print('No rooots')
+        break
 
-x = (x1 + x2)/2.0
-print('x =', '{:6.4f}'.format(x))
+if x1 != None and x2 != None:
+    x = (x1 + x2)/2.0
+    print('x =', '{:6.4f}'.format(x))
